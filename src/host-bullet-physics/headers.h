025e54b2516e811e78720d61ee3692d3fa9dded1
The-Mad-Pirate <madpirate78@yahoo.com.ar>
2014-07-06 23:17:48 -0300

    Create headers.h

---
+++ src/host-bullet-physics/headers.h
@@ -0,0 +1,9 @@
+#ifndef	HOST_BULLET_PHYSICS_H
+#define	HOST_BULLET_PHYSICS_H
+
+#include <moai-ext-test/pch.h>
+// Include MOAI etension files here
+// #include <host-bullet-physics/ .h>
+ #include <host-bullet-physics/MOAIBtVector3.h>
+
+#endif
