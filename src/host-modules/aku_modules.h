53edbf2f42f0992a893fb2a122b12d5fd97853c6
The-Mad-Pirate <madpirate78@yahoo.com.ar>
2014-07-06 23:25:24 -0300

    Added AKUBulletPhysics config def

--- src/host-modules/aku_modules.h
+++ src/host-modules/aku_modules.h
@@ -6,6 +6,10 @@
 
 #include <host-modules/aku_modules_config.h>
 
+#if AKU_WITH_BULLET_PHYSICS
+	#include <host-bullet-physics/host.h>
+#endif
+
 #if AKU_WITH_BOX2D
 	#include <moai-box2d/host.h>
 #endif
