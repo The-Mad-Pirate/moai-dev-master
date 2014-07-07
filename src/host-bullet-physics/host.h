ceb966a7d76cce1b12dfc59627a105e5c5d4cfab
The-Mad-Pirate <madpirate78@yahoo.com.ar>
2014-07-06 23:15:47 -0300

    Create host.h

---
+++ src/host-bullet-physics/host.h
@@ -0,0 +1,10 @@
+#ifndef AKU_BULLET_PHYSICS_H
+#define AKU_BULLET_PHYSICS_H
+
+#include <moai-core/host.h>
+
+AKU_API void	AKUBulletPhysicsAppFinalize					();
+AKU_API void	AKUBulletPhysicsAppInitialize				();
+AKU_API void	AKUBulletPhysicsContextInitialize			();
+
+#endif
