bcf6d723a39b72904e7afd8d36bf999bcae9124e
Leandro A. Pezzente <madpirate78@yahoo.com.ar>
2014-07-17 07:50:44 -0300

    fixed pch reference on headers.h

--- src/host-bullet-physics/headers.h
+++ src/host-bullet-physics/headers.h
@@ -1,7 +1,7 @@
 #ifndef	HOST_BULLET_PHYSICS_H
 #define	HOST_BULLET_PHYSICS_H
 
-#include <moai-ext-test/pch.h>
+#include <host-bullet-physics/pch.h>
 // Include MOAI etension files here
 // #include <host-bullet-physics/ .h>
  #include <host-bullet-physics/MOAIBtVector3.h>
